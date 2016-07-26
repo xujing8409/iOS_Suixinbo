17444883659c1f13147797d5acfb2a9828c47e78
svenyang <zhaoyang21cn@gmail.com>
2016-07-26 18:44:27 +0800

    Create EightViewController.h
    因git上传问题，从浏览器上传

---
+++ TCShow/TCAVIMDemo/TCAVIMDemo/AppUI/8路画面测试/EightViewController.h
@@ -0,0 +1,24 @@
+//
+//  EightViewController.h
+//  TCAVIMDemo
+//
+//  Created by AlexiChen on 16/7/14.
+//  Copyright © 2016年 AlexiChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GridPreview : TCAVMultiLivePreview
+
+// 九宫格画面显示
+@property (nonatomic, strong) NSMutableDictionary *renderMap;
+
+@end
+
+@interface EightRoomEngine : TCAVCallRoomEngine
+
+@end
+
+@interface EightViewController : TCAVMultiLiveViewController
+
+@end
